3883bb95889fcb33076243cbcc493b83ce0e79a3
CursoEcommerce <144336121+CursoEcommerce@users.noreply.github.com>
2023-09-07 23:14:10 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# TiendaOnline
+
+## Este es el código que utilizarás para crear tu tienda online
---
+++ components/Footer.js
@@ -0,0 +1,18 @@
+
+const Footer = () => (
+  <footer className="container xl:max-w-screen-xl mx-auto p-6 mt-8 text-center">
+    <p>
+      <a
+        href=""
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-gray-500 hover:text-current"
+      >
+        Copyright 2023
+        por Nombre Empresa
+      </a>
+    </p>
+  </footer>
+);
+
+export default Footer;
