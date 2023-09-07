b72874a429569629af7c56d1113d1cb7ac9e7364
CursoEcommerce <144336121+CursoEcommerce@users.noreply.github.com>
2023-09-07 23:37:40 +0200

    Update products.js

--- products.js
+++ products.js
@@ -11,17 +11,28 @@ const products = [
     },
   },
   {
-    id: 'price_1NneefAxZTwSHD8yBnE0dTbB',
-    name: 'Dunk Low Spartan Green',
-    price: 12000,
+    id: 'price_1NneRHAxZTwSHD8ynUzAcVRa',
+    name: 'Mesa para la Cocina',
+    price: 22000,
     currency: 'EUR',
-    image: '/zapatillas/nike-dunk-low-spartan.jpeg',
+    image: '/zapatillas/mesita-cocina.jpg',
     rating: {
-      count: 137,
+      count: 75,
       rate: 4.5,
     },
   },
   {
+    id: 'price_1NneRHAxZTwSHD8ynUzAcVRa',
+    name: 'Estantería Salón',
+    price: 50000,
+    currency: 'EUR',
+    image: '/zapatillas/estanteria-salon.jpg',
+    rating: {
+      count: 25,
+      rate: 5,
+    },
+  },
+   {
     id: 'price_1NneRHAxZTwSHD8ynUzAcVRa',
     name: 'Dunk Low Panda',
     price: 10000,
@@ -33,38 +44,27 @@ const products = [
     },
   },
   {
-    id: 'price_1NneefAxZTwSHD8yBnE0dTbB',
-    name: 'Dunk Low Spartan Green',
-    price: 12000,
+    id: 'price_1NneRHAxZTwSHD8ynUzAcVRa',
+    name: 'Mesa para la Cocina',
+    price: 22000,
     currency: 'EUR',
-    image: '/zapatillas/nike-dunk-low-spartan.jpeg',
+    image: '/zapatillas/mesita-cocina.jpg',
     rating: {
-      count: 137,
+      count: 75,
       rate: 4.5,
     },
   },
   {
     id: 'price_1NneRHAxZTwSHD8ynUzAcVRa',
-    name: 'Dunk Low Panda',
-    price: 10000,
+    name: 'Estantería Salón',
+    price: 50000,
     currency: 'EUR',
-    image: '/zapatillas/nike-nike-dunk-panda.jpg',
+    image: '/zapatillas/estanteria-salon.jpg',
     rating: {
-      count: 85,
-      rate: 4.5,
+      count: 25,
+      rate: 5,
     },
-  },
-  {
-    id: 'price_1NneefAxZTwSHD8yBnE0dTbB',
-    name: 'Dunk Low Spartan Green',
-    price: 12000,
-    currency: 'EUR',
-    image: '/zapatillas/nike-dunk-low-spartan.jpeg',
-    rating: {
-      count: 137,
-      rate: 4.5,
-    },
-  },
+  }
 ];
 
 export default products;
