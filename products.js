08f04e7d529bd13632efc184dd86f34b370dd58a
CursoEcommerce <144336121+CursoEcommerce@users.noreply.github.com>
2023-09-07 23:29:04 +0200

    Add files via upload

---
+++ products.js
@@ -0,0 +1,70 @@
+const products = [
+  {
+    id: 'price_1NneRHAxZTwSHD8ynUzAcVRa',
+    name: 'Dunk Low Panda',
+    price: 10000,
+    currency: 'EUR',
+    image: '/zapatillas/nike-nike-dunk-panda.jpg',
+    rating: {
+      count: 85,
+      rate: 4.5,
+    },
+  },
+  {
+    id: 'price_1NneefAxZTwSHD8yBnE0dTbB',
+    name: 'Dunk Low Spartan Green',
+    price: 12000,
+    currency: 'EUR',
+    image: '/zapatillas/nike-dunk-low-spartan.jpeg',
+    rating: {
+      count: 137,
+      rate: 4.5,
+    },
+  },
+  {
+    id: 'price_1NneRHAxZTwSHD8ynUzAcVRa',
+    name: 'Dunk Low Panda',
+    price: 10000,
+    currency: 'EUR',
+    image: '/zapatillas/nike-nike-dunk-panda.jpg',
+    rating: {
+      count: 85,
+      rate: 4.5,
+    },
+  },
+  {
+    id: 'price_1NneefAxZTwSHD8yBnE0dTbB',
+    name: 'Dunk Low Spartan Green',
+    price: 12000,
+    currency: 'EUR',
+    image: '/zapatillas/nike-dunk-low-spartan.jpeg',
+    rating: {
+      count: 137,
+      rate: 4.5,
+    },
+  },
+  {
+    id: 'price_1NneRHAxZTwSHD8ynUzAcVRa',
+    name: 'Dunk Low Panda',
+    price: 10000,
+    currency: 'EUR',
+    image: '/zapatillas/nike-nike-dunk-panda.jpg',
+    rating: {
+      count: 85,
+      rate: 4.5,
+    },
+  },
+  {
+    id: 'price_1NneefAxZTwSHD8yBnE0dTbB',
+    name: 'Dunk Low Spartan Green',
+    price: 12000,
+    currency: 'EUR',
+    image: '/zapatillas/nike-dunk-low-spartan.jpeg',
+    rating: {
+      count: 137,
+      rate: 4.5,
+    },
+  },
+];
+
+export default products;
